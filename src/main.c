db068e1cf9a97e8b38d7c6469e224de82098a386
bosioF <federicobosio0209@gmail.com>
2025-09-27 14:23:22 +0200

    feat: option to undo last operation

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 #define MAX_FILENAME 256
 #define MAX_BUFFER_SIZE 1048576 // 1MB
 
+unsigned char old_value = 0;
+unsigned char new_value = 0;
+size_t value_offset = 0;
+
 typedef struct {
     unsigned char *data;
     size_t size;
@@ -136,8 +140,11 @@ int hex_edit_file(HexBuffer *buffer, size_t offset, unsigned char value) {
         return 0;
     }
 
-    unsigned char old_value = buffer->data[offset];
+    old_value = buffer->data[offset];
     buffer->data[offset] = value;
+    new_value = buffer->data[offset];
+    value_offset = offset;
+
     buffer->modified = 1;
     buffer->saved = 0;
 
@@ -145,9 +152,19 @@ int hex_edit_file(HexBuffer *buffer, size_t offset, unsigned char value) {
     return 1;
 }
 
+void hex_undo(HexBuffer *buffer) {
+    if (value_offset == 0 && new_value == 0 && old_value == 0) {
+        printf("\nNo operations to undo.\n");
+    } else {
+        buffer->data[value_offset] = old_value;
+        printf("\nUndo: edit\n");
+        printf("Byte at offset %zu modified: 0x%02X -> 0x%02X\n", value_offset, new_value, old_value);
+    }   
+}
+
 void hex_search(HexBuffer *buffer, const char *hex_string) {
     if (!buffer->data || buffer->size == 0) {
-        printf("Buffer is empty\n");
+        printf("Buffer is empty\n.");
         return;
     }
 
@@ -185,16 +202,18 @@ void hex_info(HexBuffer *buffer) {
 }
 
 void show_menu() {
-    printf("\n=== HEX EDITOR ===\n");
-    printf("1. Load file (load <filename>)\n");
+    printf("\n=== CeditX ===\n");
+    printf("Command: ");
+}
+
+void help_menu() {
+    printf("\n1. Load file (load <filename>)\n");
     printf("2. Save file (save [filename])\n");
     printf("3. View (view [offset] [lines])\n");
-    printf("4. Edit byte (edit <offset> <hex_value>)\n");
+    printf("4. Edit byte (edit <offset> <hex_value>) or (edit undo) to undo last edit.\n");
     printf("5. Search pattern (search <hex_string>)\n");
     printf("6. Info (info)\n");
     printf("7. Exit (exit)\n");
-    printf("==================\n");
-    printf("Command: ");
 }
 
 int main() {
@@ -205,7 +224,7 @@ int main() {
     }
 
     char command[256];
-    char arg1[256], arg2[256], arg3[256];
+    char arg1[256], arg2[256], arg3[256], arg4[256];
 
     printf("CeditX, hex editor in C\n");
 
@@ -215,52 +234,84 @@ int main() {
         if (!fgets(command, sizeof(command), stdin)) break;
         
         command[strcspn(command, "\n")] = '\0';
-        int args = sscanf(command, "%255s %255s %255s", arg1, arg2, arg3);
+        int args = sscanf(command, "%255s %255s %255s %255s", arg1, arg2, arg3, arg4);
         
         if (args == 0) {
             continue;
         }
 
         if (strcmp(arg1, "exit") == 0 || strcmp(arg1, "quit") == 0) {
-            if (buffer->modified && buffer->saved != 1) {
-                printf("Warning, file not saved. Save? (y/n): ");
-                char response;
-                if (scanf(" %c", &response) == 1 && (response == 'y' || response == 'Y')) {
-                    hex_buffer_save(buffer, NULL);
+            if (!(args > 1)) {
+                if (buffer->modified && buffer->saved != 1) {
+                    printf("Warning, file not saved. Save? (y/n): ");
+                    char response;
+                    if (scanf(" %c", &response) == 1 && (response == 'y' || response == 'Y')) {
+                        hex_buffer_save(buffer, NULL);
+                    }
+                    while (getchar() != '\n');
                 }
-                while (getchar() != '\n');
-            }
             break;
+        } else {
+            printf("\nError: too many argouments.\n");
+        }
         } 
-        else if (strcmp(arg1, "load") == 0 && args >= 2) {
+        else if (strcmp(arg1, "load") == 0) {
+            if (!(args > 2)) {
             hex_buffer_load(buffer, arg2);
-        } 
+            }
+            else {
+                printf("\nError: too many argouments.\n");
+            }
+        }
         else if (strcmp(arg1, "save") == 0) {
-            hex_buffer_save(buffer, args >= 2 ? arg2 : NULL);
+            if (!(args >= 3)) {
+            hex_buffer_save(buffer, args == 2 ? arg2 : NULL);
+            }
+            else {
+                printf("\nError: too many argouments");
+            }
         } 
         else if (strcmp(arg1, "view") == 0) {
-            size_t offset = args >= 2 ? strtoull(arg2, NULL, 0) : 0;
-            size_t lines = args >= 3 ? strtoull(arg3, NULL, 0) : 20;
-            hex_display(buffer, offset, lines);
+            if (!(args > 1)) {
+                size_t offset = args >= 2 ? strtoull(arg2, NULL, 0) : 0;
+                size_t lines = args >= 3 ? strtoull(arg3, NULL, 0) : 20;
+                hex_display(buffer, offset, lines);
+            } else {
+                printf("\nError: too many argouments");
+            }
         }
-        else if (strcmp(arg1, "edit") == 0 && args >= 3) {
-            size_t offset = strtoull(arg2, NULL, 0);
-            unsigned char value = (unsigned char)strtoul(arg3, NULL, 16);
-            hex_edit_file(buffer, offset, value);
+        else if (strcmp(arg1, "edit") == 0) {
+            if (args < 2) {
+                printf("\nError: too few arguments\n");
+            } else if (args == 2) {
+                if (strcmp(arg2, "undo") == 0) {
+                    hex_undo(buffer);
+                } else {
+                    printf("\nError: unknown subcommand '%s'\n", arg2);
+                }
+            } else if (args == 3) {
+                size_t offset = strtoull(arg2, NULL, 0);
+                unsigned char value = (unsigned char)strtoul(arg3, NULL, 16);
+                hex_edit_file(buffer, offset, value);
+            } else {
+                printf("\nError: too many arguments.\n");
+            }
         }
-        else if (strcmp(arg1, "search") == 0 && args >= 2) {
+        else if (strcmp(arg1, "search") == 0 && args == 2) {
             hex_search(buffer, arg2);
-        }
+        } 
         else if (strcmp(arg1, "info") == 0) {
             hex_info(buffer);
         }
-        else if (strcmp(arg1, "help") == 0) {}
+        else if (strcmp(arg1, "help") == 0) {
+            help_menu();
+        }
         else {
             printf("Command not recognized. Use 'help' to see available commands.\n");
         }
     }
 
     hex_buffer_free(buffer);
-    printf("Goodbye!\n");
+    printf("\nGoodbye!\n");
     return 0;
 }
